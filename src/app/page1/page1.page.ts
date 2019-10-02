12917d9c2770037bfaba9a20a904465d9fa1f261
ozadistu <steven.ossa01@uceva.edu.co>
2019-10-01 23:51:48 -0500

    mapa en 3d

--- src/app/page1/page1.page.ts
+++ src/app/page1/page1.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 
-declare var mapboxgl: any;
+declare let mapboxgl: any;
 
 @Component({
   selector: 'app-page1',
@@ -28,15 +28,54 @@ export class Page1Page implements OnInit, AfterViewInit {
     });
 
 
-    map.on('load', function () {
+    // tslint:disable-next-line: only-arrow-functions
+    map.on('load', function() {
 
       map.resize();
 
       new mapboxgl.Marker({
         draggable: true
-        })
+      })
         .setLngLat([-76.204290, 4.066043])
         .addTo(map);
+
+      const layers = map.getStyle().layers;
+
+      let labelLayerId;
+      // tslint:disable-next-line: prefer-for-of
+      for (let i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+          labelLayerId = layers[i].id;
+          break;
+        }
+      }
+
+      map.addLayer({
+        id: '3d-buildings',
+        source: 'composite',
+        'source-layer': 'building',
+        filter: ['==', 'extrude', 'true'],
+        type: 'fill-extrusion',
+        minzoom: 15,
+        paint: {
+          'fill-extrusion-color': '#aaa',
+
+          // use an 'interpolate' expression to add a smooth transition effect to the
+          // buildings as the user zooms in
+          'fill-extrusion-height': [
+            'interpolate', ['linear'], ['zoom'],
+            15, 0,
+            15.05, ['get', 'height']
+          ],
+          'fill-extrusion-base': [
+            'interpolate', ['linear'], ['zoom'],
+            15, 0,
+            15.05, ['get', 'min_height']
+          ],
+          'fill-extrusion-opacity': .6
+        }
+      }, labelLayerId);
+
     });
 
   }
